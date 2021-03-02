503844b9e3d493434433b3698b6c4d88c224b56e
vencax <vencax@centrum.cz>
2021-03-02 15:39:29 +0100

    fileinput provides file

--- components/file.js
+++ components/file.js
@@ -1,25 +1,5 @@
-function loadAsBase64(theFile) {
-  return new Promise((resolve, reject) => {
-    var reader = new FileReader()
-
-    reader.onload = function(loadedEvent) {
-        var arrayBuffer =  new Uint8Array(loadedEvent.target.result)
-        resolve(btoa(String.fromCharCode(...arrayBuffer)))
-    }
-
-    reader.readAsArrayBuffer(theFile)
-  })
-}
-
 export default {
   props: ['config', 'disabled', 'data'],
-  methods: {
-    async onSelect (item) {
-      const data = _.pick(item, 'name', 'size', 'type')
-      data.content = await loadAsBase64(item)
-      this.$props.data[this.$props.config.name] = data
-    }
-  },
   template: `
 <validation-provider v-bind:rules="config.rules" v-slot="{ errors }">
   <b-form-group
@@ -29,7 +9,7 @@ export default {
   >
     <b-form-file
       :value="data[config.name]"
-      @input="onSelect"
+      @input="v => data[config.name] = v"
       :state="errors.length === 0"
       :disabled="disabled"
       :placeholder="config.placeholder || 'Choose a file or drop it here...'"
